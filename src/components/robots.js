4dbec3aa9580db8caf9f1ef1fd51e3e0be63a844
anushajain19 <anusha.jain19401@gmail.com>
2021-05-21 16:51:21 +0530

    first commit

---
+++ src/components/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Anusha Jain',
+      username: 'ahsay',
+      email: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Anant Jain',
+      username: 'Antu',
+      email: 'Shanna@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'Aayushi Jain',
+      username: 'Siubhi',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'Yasha Jain',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Ena Choudhary',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Varsha Jain',
+      username: 'bersa',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Pranjali Jain',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Kritika Jain',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Monarch Adlakha',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+     {
+      id: 10,
+      name: 'Aiw Moriah',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    } 
+  ];
+  
